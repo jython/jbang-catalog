7f118c7cc3ff8fcd07f4c03a88c062deb4a5d021
Werner Fouché <werner.fouche@gmail.com>
2025-05-01 12:44:21 +0200

    Added example script httpservice.py

---
+++ examples/httpservice.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env jython-cli
+
+# /// jbang
+# requires-jython = ">=2.7.4"
+# requires-java = ">=21"
+# dependencies = [
+#   "io.undertow:undertow-core:2.3.18.Final"
+# ]
+# [java]
+#   runtime-options = "-server -Xms2g -Xmx2g -XX:+UseZGC -XX:+ZGenerational"
+# ///
+
+import io.undertow.Undertow as Undertow
+import io.undertow.server.HttpHandler as HttpHandler
+import io.undertow.server.HttpServerExchange as HttpServerExchange
+
+# http://localhost:8080/
+class WebHandler(HttpHandler):
+    def handleRequest(self, httpServerExchange):
+        httpServerExchange.getResponseSender().send("<html><body><h1>Hello World from Jython/Undertow</h1></body></html>")
+
+def main():
+    webHandler = WebHandler()
+    builder = Undertow.builder()
+    undertow = builder.addHttpListener(8080, "localhost").setHandler(webHandler).build()
+    undertow.start()
+
+main()
